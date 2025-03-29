009a16202dba1b57882ced85287198b3d73d66d5
Nazar <nazartalaievych@gmail.com>
2025-03-29 22:59:28 +0200

    Create and adapt the header section of the main page.

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { abort } from 'node:process'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        catalog: resolve(__dirname, 'catalog.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        about: resolve(__dirname, 'about.html'),
+    },
+    },
+  },
+})
