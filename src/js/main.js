a95e88b0e03c5f27b4d1b8e5c2bf20f3f2c0a3e0
Nazar <nazartalaievych@gmail.com>
2025-03-31 16:35:51 +0300

    Style the works section with responsive Swiper slider adjustments

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,34 @@
 /* import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css"; */
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "/src/sass/style.scss";
+
+try {
+    new Swiper(".works__slider", {
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: ".icon-right-open",
+            prevEl: ".icon-left-open",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                spaceBetween: 35,
+            },
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) {}
