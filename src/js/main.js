30120b1880b5bdea20b8e09876ffae8adbe238e3
Nazar <nazartalaievych@gmail.com>
2025-04-01 14:50:29 +0300

    Implement the functionality of the burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,20 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "/src/sass/style.scss";
 
+const burger = document.querySelector(".burger"),
+    menu = document.querySelector(".header__menu"),
+    close = document.querySelector(".header__menu_close");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
+});
+
 try {
     new Swiper(".works__slider", {
         slidesPerView: 1,
