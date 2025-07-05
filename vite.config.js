d1750b5259f03b584f33ed1ee39a2369f9089aee
Nazar <nazartalaievych@gmail.com>
2025-07-05 22:42:11 +0300

    last commit and deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { dirname, resolve } from "node:path";
-import { abort } from "node:process";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -16,4 +16,33 @@ export default defineConfig({
             },
         },
     },
+    plugins: [
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 70,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: "removeViewBox",
+                    },
+                    {
+                        name: "removeEmptyAttrs",
+                        active: false,
+                    },
+                ],
+            },
+        }),
+    ],
 });
